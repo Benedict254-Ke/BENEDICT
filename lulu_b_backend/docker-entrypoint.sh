a06d555ba8f1b81eaec9fe44ca7d84ad6be9e11d
Benedict254-Ke <lulub8929@gmail.com>
2025-11-18 18:02:18 +0300

    Setup Nginx + PHP-FPM in single container for Render

---
+++ lulu_b_backend/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Starting application services..."
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf#!/bin/bash
+set -e
+
+echo "Starting application services..."
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
