9f060c5db4137cfb58ee9086b9e114e2322b86d7
Benedict254-Ke <lulub8929@gmail.com>
2025-11-19 13:40:15 +0300

    Setup Nginx + PHP-FPM in single container for Render

--- lulu_b_backend/start.sh
+++ lulu_b_backend/start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "[$(date)] === Starting Application ===" > /proc/1/fd/1
+echo "Starting PHP-FPM..."
+php-fpm &
 
-echo "[$(date)] Starting PHP-FPM..." > /proc/1/fd/1
-/usr/local/sbin/php-fpm -D
-
-echo "[$(date)] Sleeping 3 seconds..." > /proc/1/fd/1
-sleep 3
-
-echo "[$(date)] Starting Nginx..." > /proc/1/fd/1
-exec /usr/sbin/nginx -g "daemon off;"
+echo "Starting Nginx..."
+exec nginx -g "daemon off;"
