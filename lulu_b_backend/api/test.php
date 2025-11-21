68db037d121fa8988626b2ddac6dece6a2ddd3c4
Benedict254-Ke <lulub8929@gmail.com>
2025-11-21 09:43:46 +0300

    Fix CORS origin spelling for frontend

---
+++ lulu_b_backend/api/test.php
@@ -0,0 +1,18 @@
+<?php
+// test.php - Simple test endpoint
+header("Access-Control-Allow-Origin: https://benedict-personal-portifolio.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+echo json_encode([
+    "success" => true,
+    "message" => "Backend is working!",
+    "timestamp" => date('Y-m-d H:i:s')
+]);
+?>
